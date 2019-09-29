12dc6cab08a14fc2e3f96f576848ade26cda1b87
Adam Keune <akeune23@gmail.com>
2019-09-29 13:20:20 -0500

    Add @board, find_piece, and move methods (incomplete)

--- checkers.rb
+++ checkers.rb
@@ -17,4 +17,21 @@ class Checkers
   def board
     @board
   end
+
+  def find_piece(symbol, number)
+    symbol_array = []
+    @board.each do |row|
+      row.each do |i|
+        symbol_array << i if i == symbol
+      end
+    end
+    symbol_array.fetch(number - 1)
+  end
+
+  def move
+    # find all remaining x's and o's and place in arrays
+    # find specified piece
+    # move piece specified direction
+    # return new game board
+  end
 end
--- checkers_spec.rb
+++ checkers_spec.rb
@@ -16,3 +16,28 @@ RSpec.describe "board" do
     ])
   end
 end
+
+RSpec.describe "find_piece" do
+  it "should return the specified game piece" do
+    game = Checkers.new
+
+    expect(game.find_piece("x", 12)).to eq(game.board[2][7])
+  end
+end
+
+# RSpec.describe "move" do
+#   it "should return game board with specified checker moved" do
+#     game = Checkers.new
+
+#     expect(game.move("x")).to eq([
+#       [" ", "x", " ", "x", " ", "x", " ", "x"],
+#       ["x", " ", "x", " ", "x", " ", "x", " "],
+#       [" ", "x", " ", "x", " ", "x", " ", " "],
+#       [" ", " ", " ", " ", " ", " ", "x", " "],
+#       [" ", " ", " ", " ", " ", " ", " ", " "],
+#       ["o", " ", "o", " ", "o", " ", "o", " "],
+#       [" ", "o", " ", "o", " ", "o", " ", "o"],
+#       ["o", " ", "o", " ", "o", " ", "o", " "],
+#     ])
+#   end
+# end

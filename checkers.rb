6512d7f2c4250af5ef043aa7d17cedb6fdc4f251
Adam Keune <akeune23@gmail.com>
2019-10-01 13:41:01 -0500

    Fix 'jump' method, add test for backwards motion

--- checkers.rb
+++ checkers.rb
@@ -1,6 +1,5 @@
 class Checkers
-  attr_reader :current_player
-  attr_accessor :board
+  attr_accessor :board, :current_player
 
   def initialize
     @board = [
@@ -15,10 +14,6 @@ class Checkers
       ["8", "o", " ", "o", " ", "o", " ", "o", " "],
     ]
     @current_player = "x"
-  end
-
-  def board
-    @board
   end
 
   def toggle_player(player)
@@ -37,7 +32,7 @@ class Checkers
   end
 
   def move(row1, column1, row2, column2)
-    if @board[row1][column1] == @current_player
+    if @board[row1][column1].downcase == @current_player
       checker = @board[row1][column1]
 
       if @board[row1][column1] == @board[row1][column1].upcase
@@ -46,11 +41,10 @@ class Checkers
            (column1 - column2).abs == 1
           @board[row1][column1] = " "
           @board[row2][column2] = checker
-          # @board
+          toggle_player(current_player)
+          @board
         else
-          puts
           pp "Invalid move"
-          puts
         end
       elsif @board[row1][column1] == "x"
         if @board[row2][column2] == " " &&
@@ -58,44 +52,45 @@ class Checkers
            (column1 - column2).abs == 1
           @board[row1][column1] = " "
           @board[row2][column2] = checker
-          # @board
+          toggle_player(current_player)
+          @board
         else
-          puts
           pp "Invalid move"
-          puts
         end
       elsif @board[row1][column1] == "o"
         if @board[row2][column2] == " " &&
            row1 - row2 == 1 &&
            (column1 - column2).abs == 1
           @board[row1][column1] = " "
+          toggle_player(current_player)
           @board[row2][column2] = checker
-          # @board
+          @board
         else
-          puts
           pp "Invalid move"
-          puts
         end
       end
-      toggle_player(current_player)
-      @board
+      # @board
     else
-      puts
       pp "Invalid move"
-      puts
     end
   end
 
-  def jump(current_player, row1, column1, row2, column2) # refactor, create variables for jumped spaces
-    checker = @board[row1][column1]
+  def jump(current_player, row1, column1, row2, column2)
+    p row1
+    p column1
+    checker = @board[row1][column1] # this isn't registering, returns " "
+    p checker # -> " "
 
+    jumped_row = ((row1 + row2) / 2)
+    jumped_column = ((column1 + column2) / 2)
     if (row1 - row2).abs == 2 &&
        (column1 - column2).abs == 2 &&
-       @board[((row1 + row2) / 2)][((column1 + column2) / 2)] != " " &&
-       @board[((row1 + row2) / 2)][((column1 + column2) / 2)] != current_player
+       @board[jumped_row][jumped_column] != " " &&
+       @board[jumped_row][jumped_column] != current_player
       @board[row1][column1] = " "
-      @board[((row1 + row2) / 2)][((column1 + column2) / 2)] = " "
+      @board[jumped_row][jumped_column] = " "
       @board[row2][column2] = checker
+      toggle_player(current_player)
       @board
     else
       "Invalid jump"
--- game_runner.rb
+++ game_runner.rb
@@ -2,7 +2,7 @@ require "./checkers"
 
 game = Checkers.new
 
-while game.board.flatten.any?("x") || game.board.flatten.any?("o")
+while (game.board.flatten.any?("x") || game.board.flatten.any?("X")) && (game.board.flatten.any?("o") || game.board.flatten.any?("O"))
   pp game.board
 
   puts "Player #{game.current_player}, which piece do you want to move?"
@@ -19,6 +19,8 @@ while game.board.flatten.any?("x") || game.board.flatten.any?("o")
   puts "Column?"
   column2 = gets.chomp.to_i
 
-  game.move(row1, column1, row2, column2)
+  game.move(row1, column1, row2, column2) if (row1 - row2).abs == 1
+  game.jump(game.current_player, row1, column1, row2, column2) if (row1 - row2).abs == 2
+
   # game.toggle_player(game.current_player)
 end

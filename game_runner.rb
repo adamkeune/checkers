845ee9148526574aaf9af8a7ac4086f06df87f49
Adam Keune <akeune23@gmail.com>
2019-10-04 12:57:10 -0500

    Refactor 'jump' call with do...while loop

--- game_runner.rb
+++ game_runner.rb
@@ -22,26 +22,24 @@ while (game.board.flatten.any?("x") || game.board.flatten.any?("X")) && (game.bo
     game.move(row1, column1, row2, column2)
     pp game.king
   elsif (row1 - row2).abs == 2 && (column1 - column2).abs == 2
-    game.jump(game.current_player, row1, column1, row2, column2)
-    pp game.king
-
-    puts "Jump again? (Y/N)"  # HOW DO I TEST THIS?
-    jump_again = gets.chomp
-
-    while jump_again.downcase == "y"
-      row1 = row2
-      puts "Row?"
-      row2 = gets.chomp.to_i
-
-      column1 = column2
-      puts "Column?"
-      column2 = gets.chomp.to_i
-
+    loop do
       game.jump(game.current_player, row1, column1, row2, column2)
       pp game.king
 
       puts "Jump again? (Y/N)"
       jump_again = gets.chomp
+
+      if jump_again.downcase == "y"
+        row1 = row2
+        puts "Row?"
+        row2 = gets.chomp.to_i
+
+        column1 = column2
+        puts "Column?"
+        column2 = gets.chomp.to_i
+      else
+        break
+      end
     end
     game.toggle_player(game.current_player)
   else
